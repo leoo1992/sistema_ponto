ec3036f42f853f25616451793b8fd87cf1830e1c
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-02 19:08:52 -0300

    use Navigate

--- src/hooks/useSubmit.tsx
+++ src/hooks/useSubmit.tsx
@@ -1,28 +1,29 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
-import {FormData} from "../types";
-
+import { useNavigate } from 'react-router-dom';
+import { FormData } from "../types";
 
 export function useSubmit() {
-    const { register, handleSubmit } = useForm<FormData>();
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<FormData>();
 
-    const onSubmit: SubmitHandler<FormData> = (data) => {
-        if (!data || !data.user || ! data.pass) {
-            return
-        }
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    // if (!data || !data.user || !data.pass) {
+    //     return;
+    // }
 
-        if (data.pass.length < 11 || data.pass.includes(' ')) {
-            alert('senha deve ter 11 digitos');
-            return
-        }
+    // if (data.pass.length < 11 || data.pass.includes(' ')) {
+    //     alert('A senha deve ter 11 dígitos');
+    //     return;
+    // }
 
-        console.log(data);
+    console.log(data);
 
-        window.location.href = '/home';
-    };
+    navigate('/home');
+  };
 
   return {
     onSubmit,
     register,
     handleSubmit
-  }
+  };
 }
