fc2f4056c13ff62c35ecad488d79881c76eb085a
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-22 11:54:20 -0300

    centralize imports of all pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
+import LossPassword from './LossPassword';
+import ErrorPage from './ErrorPage';
+import RouteIncorrect from './RouteIncorrect';
+import RegisterPage from './RegisterPage';
+
+export {
+  LoginPage,
+  HomePage,
+  LossPassword,
+  ErrorPage,
+  RouteIncorrect,
+  RegisterPage,
+};
