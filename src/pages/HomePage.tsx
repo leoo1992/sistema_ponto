df8f3cfd3ed02ab2a27364c3b80d9ce18eed8a72
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-22 11:54:00 -0300

    refactor pages to use navbar and footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,13 @@
 import CardDayRegisters from '../components/Home/DayRegisters/CardDayRegisters';
-import Footer from '../components/Home/Footer/Footer';
-import Navbar from '../components/Home/Nav/Navbar';
 import StatsBar from '../components/Home/Stats/StatsBar';
 import { useStats } from '../hooks/useStats';
 
 export default function HomePage() {
   const statsUse = useStats();
   return (
-    <div className="-z-50 flex flex-col min-h-screen min-w-screen transition-all duration-500 ease-in-out m-0 p-0">
-      <Navbar />
-      <div className="flex-grow flex justify-center items-start m-0 pt-5">
-        <CardDayRegisters statsUse={statsUse} />
-        <StatsBar statsUse={statsUse} />
-      </div>
-      <Footer />
+    <div className="z-0 flex-grow flex justify-center items-start m-0 pt-5">
+      <CardDayRegisters statsUse={statsUse} />
+      <StatsBar statsUse={statsUse} />
     </div>
   );
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,90 +1,81 @@
-import Footer from '../components/Home/Footer/Footer';
-import Navbar from '../components/Home/Nav/Navbar';
-
 export default function RegisterPage() {
   return (
-    <div className="-z-50 flex flex-col min-h-screen min-w-screen transition-all duration-500 ease-in-out m-0 p-0">
-      <Navbar />
-      <div className="flex-grow flex justify-center items-start m-0 pt-5">
-        <div
-          className="card shrink-0 max-w-sm shadow-sm bg-gradient-to-b
+    <div className="flex-grow flex justify-center items-start m-0 pt-5">
+      <div
+        className="card shrink-0 max-w-sm shadow-sm bg-gradient-to-b
      from-slate-100 via-white to-white  shadow-primary"
-        >
-          <form className="card-body p-5 sm:p-8 sm:w-96">
-            <div className="form-control">
-              <h1 className="text-primary font-bold text-center text-lg">
-                Cadastro
-              </h1>
-              <label className="label">
-                <span className="label-text text-primary font-bold">Nome</span>
-              </label>
-              <input
-                type="text"
-                name="name"
-                placeholder="Nome"
-                className="input input-bordered shadow-primary shadow-sm text-primary input-md"
-                maxLength={20}
-                required
-              />
-              <label className="label">
-                <span className="label-text text-primary font-bold">
-                  E-mail
-                </span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="E-mail"
-                className="input input-bordered shadow-primary shadow-sm text-primary input-md"
-                maxLength={20}
-                required
-              />
-              <label className="label">
-                <span className="label-text text-primary font-bold">Senha</span>
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="Senha"
-                className="input input-bordered shadow-primary shadow-sm text-primary input-md"
-                maxLength={11}
-                required
-              />
-              <label className="label">
-                <span className="label-text text-primary font-bold">
-                  Telefone
-                </span>
-              </label>
-              <input
-                type="text"
-                name="telefone"
-                placeholder="Telefone"
-                className="input input-bordered shadow-primary shadow-sm text-primary input-md"
-                maxLength={11}
-                required
-              />
-              <label className="label">
-                <span className="label-text text-primary font-bold">
-                  Tipo de usuário
-                </span>
-              </label>
-              <select
-                defaultValue={0}
-                className="input input-bordered shadow-primary shadow-sm text-primary input-md"
-                required
-              >
-                <option disabled value={0}>
-                  Selecione o tipo
-                </option>
-                <option value={1}>Administrador</option>
-                <option value={2}>Usuário</option>
-              </select>
-              <button className="btn btn-primary mt-5">Registrar</button>
-            </div>
-          </form>
-        </div>
+      >
+        <form className="card-body p-5 sm:p-8 sm:w-96">
+          <div className="form-control">
+            <h1 className="text-primary font-bold text-center text-lg">
+              Cadastro
+            </h1>
+            <label className="label">
+              <span className="label-text text-primary font-bold">Nome</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Nome"
+              className="input input-bordered shadow-primary shadow-sm text-primary input-md"
+              maxLength={20}
+              required
+            />
+            <label className="label">
+              <span className="label-text text-primary font-bold">E-mail</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              className="input input-bordered shadow-primary shadow-sm text-primary input-md"
+              maxLength={20}
+              required
+            />
+            <label className="label">
+              <span className="label-text text-primary font-bold">Senha</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              className="input input-bordered shadow-primary shadow-sm text-primary input-md"
+              maxLength={11}
+              required
+            />
+            <label className="label">
+              <span className="label-text text-primary font-bold">
+                Telefone
+              </span>
+            </label>
+            <input
+              type="text"
+              name="telefone"
+              placeholder="Telefone"
+              className="input input-bordered shadow-primary shadow-sm text-primary input-md"
+              maxLength={11}
+              required
+            />
+            <label className="label">
+              <span className="label-text text-primary font-bold">
+                Tipo de usuário
+              </span>
+            </label>
+            <select
+              defaultValue={0}
+              className="input input-bordered shadow-primary shadow-sm text-primary input-md"
+              required
+            >
+              <option disabled value={0}>
+                Selecione o tipo
+              </option>
+              <option value={1}>Administrador</option>
+              <option value={2}>Usuário</option>
+            </select>
+            <button className="btn btn-primary mt-5">Registrar</button>
+          </div>
+        </form>
       </div>
-      <Footer />
     </div>
   );
 }
