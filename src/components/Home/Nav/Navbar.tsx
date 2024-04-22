da4c8b9fb610a9c364c342948ba0dbd7219a8767
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-22 11:53:09 -0300

    remove coments

--- src/components/Home/Nav/Navbar.tsx
+++ src/components/Home/Nav/Navbar.tsx
@@ -1,14 +1,12 @@
 import LeftMenu from './LeftMenu';
 import UserMenu from './UserMenu';
-// import ThemeToogleHome from "./ThemeToogleHome";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-primary-content hover:opacity-100 shadow-lg">
+    <div className="navbar bg-primary-content hover:opacity-100 shadow-lg z-50">
       <LeftMenu />
       <div className="navbar-center sm:text-xl font-bold">PontoAdm</div>
       <div className="navbar-end m-0 p-0">
-        {/* <ThemeToogleHome /> */}
         <UserMenu />
       </div>
     </div>
