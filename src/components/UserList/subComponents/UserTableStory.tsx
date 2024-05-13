15634cfb9f06a838915cac1446fb24f2e9b6ca23
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-05-13 20:59:19 -0300

    deploy

--- src/components/UserList/subComponents/UserTableStory.tsx
+++ src/components/UserList/subComponents/UserTableStory.tsx
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { getUserDataTableColumns } from "../../../utils/UserList/getUserDataTableColumns.util";
 import { NoDataComponent } from "./TableComponent/NoDataComponent";
 import { ProgressComponent } from "./TableComponent/ProgressComponent";
-import { SubHeaderComponent } from "./TableComponent/subHeaderComponent";
+import { subHeaderComponent } from "./TableComponent/SubHeaderComponent";
 import DataTable from "react-data-table-component";
 import UserListGET from "../../../services/UserList/UserListGET";
 import Pagination from "./TableComponent/Pagination";
@@ -248,7 +248,7 @@ export const UserTableStory = ({
         progressComponent={ProgressComponent}
         noHeader={noHeader}
         subHeader={subHeader}
-        subHeaderComponent={SubHeaderComponent}
+        subHeaderComponent={subHeaderComponent}
         subHeaderAlign={subHeaderAlign}
         subHeaderWrap={subHeaderWrap}
         noContextMenu={noContextMenu}
