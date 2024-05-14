c85136295d0b8bd1b77f55ecf35101123c1e5277
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-05-13 21:26:43 -0300

    deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import replace from "@rollup/plugin-replace";
-import terser from "@rollup/plugin-terser";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    port: 5173,
-  },
-  plugins: [
-    react(),
-    replace({
-      preventAssignment: true,
-    }),
-    terser(),
-  ],
-  build: {
-    rollupOptions: {
-      external: ["styled-components"],
-    },
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['styled-components'],
   },
 });
