4055054a83490011f08163f3c406ea01f8c7c4a0
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-05-14 23:08:11 -0300

    deploy remove styled components

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['styled-components'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['styled-components'],
-    },
-  },
 });
