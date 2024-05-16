8bc4926d8c4135bca77c20a83ad93907b5e317e5
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-05-16 18:06:41 -0300

    add btn work time register

--- src/pages/PrivatePages/HomePage.tsx
+++ src/pages/PrivatePages/HomePage.tsx
@@ -1,9 +1,29 @@
 import Waves from "../../components/Waves";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [btnState, setBtnState] = useState(true);
+
   return (
-    <div className="z-0 m-0 flex flex-grow items-start justify-center py-5">
+    <div className="z-0 m-0 flex flex-grow flex-col items-center py-5">
       HOME
+      <div className="flex justify-end w-full p-3">
+        {btnState ? (
+          <button
+            onClick={() => setBtnState(!btnState)}
+            className={`btn glass btn-primary m-2 w-36 bg-primary text-white`}
+          >
+            Registrar Entrada
+          </button>
+        ) : (
+          <button
+            onClick={() => setBtnState(!btnState)}
+            className={`btn glass btn-error m-2 w-36 bg-error text-white`}
+          >
+            Registrar Saida
+          </button>
+        )}
+      </div>
       <Waves />
     </div>
   );
