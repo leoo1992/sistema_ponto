35a7bf60f7629cf7e9c963755a17b312b1a06d6c
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-05-17 18:17:48 -0300

    prepare posts to create sector and positions

---
+++ src/services/RegisterPosition/postCreatePosition.tsx
@@ -0,0 +1,31 @@
+import Cookies from "js-cookie";
+import { notifySuccess } from "../../components/Toasts/ToastSuccess";
+import { notifyError } from "../../components/Toasts/ToastError";
+
+export default async function postCreatePosition(data : any,
+  navigate: any,
+) {
+
+  const NewSectorURL = import.meta.env.TEST;
+  //TODO AQUIIIIII
+  
+  try {
+    const response = await fetch(NewSectorURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("AuthToken")}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      notifySuccess({ text: "Setores criados" });
+      navigate("/home");
+    } else {
+      notifyError({ text: "Erro ao criar setores" });
+    }
+  } catch (error) {
+    notifyError({ text: "Erro ao criar setores" });
+  }
+}
