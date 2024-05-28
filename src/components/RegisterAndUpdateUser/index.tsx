d584a108b4580cee062d6b72a3097469f5ececac
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-05-28 15:41:39 -0300

    deploy adjusted

--- src/components/RegisterAndUpdateUser/index.tsx
+++ src/components/RegisterAndUpdateUser/index.tsx
@@ -1,23 +1,8 @@
-import {
-  HiKey,
-  HiLockClosed,
-  HiOfficeBuilding,
-  HiIdentification,
-  HiPhone,
-  HiMail,
-  HiUser,
-} from "react-icons/hi";
-import { BsWrench } from "react-icons/bs";
-import { Input } from "./subComponents/Input";
-import { Select } from "./subComponents/Select";
-import { ErrorMessage } from "./subComponents/ErrorMsgs";
 import { useUserForm } from "../../hooks/User/useUserForm";
 import getRole from "../../services/Role/getRole";
 import getPosition from "../../services/Position/getPosition";
 import getSector from "../../services/Sector/getSector";
-import { validarCPF } from "../../utils/CreateUser/validarCPF";
 import { MaskCPF } from "../../utils/CreateUser/maskCPF";
-import { MaskPhone } from "../../utils/CreateUser/MaskPhone";
 import UserFormEffects from "../../hooks/Effects/User/UserFormEffects";
 import UserFormContainer from "./subComponents/UserFormContainer";
 export default function index() {
