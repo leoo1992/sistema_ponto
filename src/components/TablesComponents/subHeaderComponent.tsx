72d669411ea5329aca081ddee18079f25fa02568
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-05-31 17:55:23 -0300

    add filter

--- src/components/TablesComponents/subHeaderComponent.tsx
+++ src/components/TablesComponents/subHeaderComponent.tsx
@@ -1,9 +1,54 @@
 import { HiSearch, HiUserAdd } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export const subHeaderComponent = (link:string) => {
+  const [filtro, setFiltro] = useState('ativos');
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (event: any) => {
+    setFiltro(event.target.value);
+    handleClose();
+    
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+
   return (
     <div className="flex-col-2 flex w-full items-center justify-end gap-2 text-center text-xs">
+      <div className="border rounded-lg p-0 justify-between">
+        <Select
+          labelId="select-filter"
+          id="id-select-filter"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={filtro}
+          label="Filtro"
+          onChange={handleChange}
+          size="small"
+          defaultValue="ativos"
+        >
+          <MenuItem value="ativos">
+            <em>Ativos</em>
+          </MenuItem>
+          <MenuItem value="inativos">
+            <em>Inativos</em>
+          </MenuItem>
+          <MenuItem value="ambos">
+            <em>Ambos</em>
+          </MenuItem>
+        </Select>
+      </div>
       <Link
         to={link}
         className="text-md btn btn-circle glass btn-primary
